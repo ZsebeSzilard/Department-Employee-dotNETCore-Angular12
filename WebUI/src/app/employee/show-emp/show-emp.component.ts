d04224d639d924b37c025fc8908c1c5b3754fd29
ZsebeSzilard <zsebeszilard@gmail.com>
2021-08-14 12:21:10 +0300

    AddWebUI

---
+++ WebUI/src/app/employee/show-emp/show-emp.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+
+@Component({
+  selector: 'app-show-emp',
+  templateUrl: './show-emp.component.html',
+  styleUrls: ['./show-emp.component.css']
+})
+export class ShowEmpComponent implements OnInit {
+
+  constructor(private service: SharedService) { }
+
+  EmployeeList: any = [];
+
+  ModalTitle: string = "";
+  ActivateAddEditEmpComp: boolean = false;
+  emp: any;
+
+  ngOnInit(): void {
+    this.refreshEmployeeList();
+  }
+
+  addClick() {
+    this.emp = {
+      EmployeeId: 0,
+      EmployeeName: "",
+      Department: "",
+      DateOfJoining: "",
+      PhotoFileName: "anonymous.png"
+    }
+    this.ModalTitle = "Add Employee";
+    this.ActivateAddEditEmpComp = true;
+  }
+
+  editClick(item: any) {
+    this.emp = item;
+    this.ModalTitle = "Edit Employee";
+    this.ActivateAddEditEmpComp = true;
+  }
+
+  deleteClick(item: any) {
+    if (confirm('Are you sure?')) {
+      this.service.deleteEmployee(item.EmployeeId).subscribe(data => {
+        alert(data.toString());
+        this.refreshEmployeeList();
+      })
+    }
+  }
+
+  closeClick() {
+    this.ActivateAddEditEmpComp = false;
+    this.refreshEmployeeList();
+  }
+
+  refreshEmployeeList() {
+    this.service.getEmpList().subscribe(data => {
+      data.forEach((item) => {
+        item.DateOfJoining = item.DateOfJoining.slice(0, 10);
+      });
+      this.EmployeeList = data;
+    });
+  }
+}
